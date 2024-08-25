e45a7bca66c65a1d49172285aac3586f6310a8af
Shriram Bhardwaj <shrirambhardwaj68@gmail.com>
2024-08-25 19:22:23 +0530

    Added

--- config/swagger.config.js
+++ config/swagger.config.js
@@ -1,17 +1,11 @@
+// swaggerConfig.js
 import swaggerUi from "swagger-ui-express";
 import YAML from 'yamljs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Adjust the path based on the actual file location
-const swaggerPath = path.join(__dirname, '../docs', 'swagger.yaml');
-const swaggerDocument = YAML.load(swaggerPath);
+const swaggerDocument = YAML.load('./config/swagger.yaml');
 
 const setupSwagger = (app) => {
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 };
 
 export default setupSwagger;
