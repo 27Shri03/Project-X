efe626498e842d360f1392b1d1783d0c7bcacd69
Shriram Bhardwaj <shrirambhardwaj68@gmail.com>
2024-08-30 22:08:31 +0530

    updated the socket docs

--- API/Controllers/chat.controllers.js
+++ API/Controllers/chat.controllers.js
@@ -3,13 +3,9 @@ import { Conversation } from "../../Models/conversation.model.js";
 
 export const getMessages = async (req, res) => {
     try {
-        const userId = req.user.userId;
-        const { limit = 20, page = 1, friendId } = req.query;
-
-        let conversation = await Conversation.findOne({
-            participants: { $all: [userId, friendId], $size: 2 }
-        });
-
+        const { limit = 20, page = 1, conversationId } = req.query;
+        let conversation = await Conversation.findById(conversationId);
+        console.log(conversationId);
         if (!conversation) {
             res.status(404).json({ message: "Conversation not found please make the User friend first" });
         }
@@ -24,10 +20,8 @@ export const getMessages = async (req, res) => {
             messages,
             totalMessages,
             hasMore,
-            conversationId: conversation._id
         });
     } catch (error) {
         res.status(500).json({ message: 'Internal server error', error: error.message });
     }
 };
-
