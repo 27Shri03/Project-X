b1deadc3c179b9a238d9e431adfc846fd4ba8313
Shriram Bhardwaj <shrirambhardwaj68@gmail.com>
2024-08-31 23:30:29 +0530

    Deployed to azure

--- API/Controllers/user.controllers.js
+++ API/Controllers/user.controllers.js
@@ -77,8 +77,14 @@ export const acceptFriendRequest = async (req, res) => {
             UID: myinfo._id,
             conversationId: conversation._id
         }
+        const API_Payload = {
+            username: username,
+            photo: otherUser.photo,
+            UID: otherUser._id,
+            conversationId: conversation._id
+        }
         emitToUser(otherUser._id.toString(), EVENTS.ACCEPTEDFRIENDREQUEST, payload);
-        return res.status(200).json({ message: "Friend Request accepted Successfully" });
+        return res.status(200).json({ message: "Friend Request accepted Successfully", data: API_Payload });
 
     } catch (error) {
         return res.status(500).json({ message: "Internal server error", error: error.message });
